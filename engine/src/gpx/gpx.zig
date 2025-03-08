7e9665650bb1115eaf495832e66e80a22eedadd1
Ihor L <k4k7005@gmail.com>
2025-03-09 00:42:44 +0200

    engine: gpx: impl parsing time string to timestmap

--- engine/src/gpx/gpx.zig
+++ engine/src/gpx/gpx.zig
@@ -6,7 +6,25 @@ const fmt = std.fmt;
 
 const indexOf = mem.indexOfPosLinear;
 
-pub const Error = error{InvalidGpx};
+pub const Error = error{ InvalidGpx, InvalidGpxTime };
+
+pub const CompactGpx = struct {
+    lat: []f64,
+    lon: []f64,
+    time: []u32,
+    ele: ?[]i32,
+    allocator: mem.Allocator,
+
+    // pub fn init(allocator: mem.Allocator, size: usize) !CompactGpx {
+    //     return .{
+    //         .lat = try allocator.alloc(f64, size),
+    //         .lon = try allocator.alloc(f64, size),
+    //         .time = try allocator.alloc(u32, size),
+    //         .ele = try allocator.alloc(i32, size),
+    //         .allocator = allocator,
+    //     };
+    // }
+};
 
 pub const Gpx = struct {
     name: []const u8,
@@ -28,7 +46,7 @@ pub const Gpx = struct {
     pub const Trkpt = struct {
         lat: f64,
         lon: f64,
-        ele: f16,
+        ele: f16, // fixme change to i32 or i16 or u16?
         time: u32,
 
         pub const tagname = "trkpt";
@@ -39,24 +57,24 @@ pub fn parse(a: mem.Allocator, gpx: []const u8) !Gpx {
     var result = Gpx.init(a);
     errdefer result.deinit();
 
-    result.name = tagContent(gpx, "name") orelse return Error.InvalidGpx;
-    result.type = tagContent(gpx, "type") orelse return Error.InvalidGpx;
+    result.name = try tagContent(gpx, "name");
+    result.type = try tagContent(gpx, "type");
 
     var pos: usize = 0;
     while (true) {
-        const idx, const trkpt = tagAndIndex(gpx[pos..], Gpx.Trkpt.tagname) orelse
+        const idx, const trkpt = try tagAndIndex(gpx[pos..], Gpx.Trkpt.tagname) orelse
             return result;
         assert(trkpt.len >= 2 * Gpx.Trkpt.tagname.len + "<></>".len);
-        const lat = attr(trkpt, "lat") orelse return Error.InvalidGpx;
-        const lon = attr(trkpt, "lon") orelse return Error.InvalidGpx;
-        const ele = tagContent(trkpt, "ele") orelse return Error.InvalidGpx;
-        const time = tagContent(trkpt, "time") orelse return Error.InvalidGpx;
+        const lat = try attr(trkpt, "lat");
+        const lon = try attr(trkpt, "lon");
+        const ele = try tagContent(trkpt, "ele");
+        const time = try tagContent(trkpt, "time");
 
         try result.track.append(.{
             .lat = fmt.parseFloat(f64, lat) catch return Error.InvalidGpx,
             .lon = fmt.parseFloat(f64, lon) catch return Error.InvalidGpx,
             .ele = fmt.parseFloat(f16, ele) catch return Error.InvalidGpx,
-            .time = @intCast(time.len), // FIXME
+            .time = timeStrToTimestamp(time) catch return Error.InvalidGpxTime,
         });
 
         pos += idx + trkpt.len;
@@ -84,29 +102,29 @@ test parse {
         \\  </trgseg>
         \\</trk>
     ;
-
     const gpx = try parse(t.allocator, gpx_content);
     defer gpx.deinit();
 
     try t.expect(mem.eql(u8, gpx.name, "Testing 🏝️"));
     try t.expect(mem.eql(u8, gpx.type, "cycling"));
     try t.expect(gpx.track.items.len == 2);
+    try t.expectError(Error.InvalidGpx, parse(t.allocator, gpx_content[0 .. gpx_content.len / 2]));
 }
 
 pub fn tagAndIndex(
     gpx: []const u8,
     comptime tag_name: []const u8,
-) ?struct { usize, []const u8 } {
+) !?struct { usize, []const u8 } {
     const from = indexOf(u8, gpx, 0, "<" ++ tag_name) orelse
         return null;
     const closing_tag = "</" ++ tag_name ++ ">";
     const to = indexOf(u8, gpx, from + 1, closing_tag) orelse
-        return null;
+        return Error.InvalidGpx;
     return .{ from, gpx[from .. to + closing_tag.len] };
 }
 
-pub fn tag(gpx: []const u8, comptime tag_name: []const u8) ?[]const u8 {
-    _, const tag_str = tagAndIndex(gpx, tag_name) orelse return null;
+pub fn tag(gpx: []const u8, comptime tag_name: []const u8) !?[]const u8 {
+    _, const tag_str = try tagAndIndex(gpx, tag_name) orelse return Error.InvalidGpx;
     return tag_str;
 }
 
@@ -119,21 +137,21 @@ test tag {
         \\</trkpt>
     ;
 
-    const ele = tag(gpx, "ele");
-    const time = tag(gpx, "time");
-    const foo = tag(gpx, "foo");
+    const ele = try tag(gpx, "ele");
+    const time = try tag(gpx, "time");
+    const foo = try tag(gpx, "foo");
 
     try testing.expect(mem.eql(u8, ele.?, "<ele>126.0</ele>"));
     try testing.expect(mem.eql(u8, time.?, "<time>2022-01-07T09:19:57Z</time>"));
     try testing.expect(mem.eql(u8, foo.?, "<foo attr1=\"val\">hello</foo>"));
-    try testing.expect(tagContent("missingtag", gpx) == null);
+    try testing.expectError(Error.InvalidGpx, tagContent("missingtag", gpx));
 }
 
-pub fn tagContent(gpx: []const u8, comptime tag_name: []const u8) ?[]const u8 {
-    const from = indexOf(u8, gpx, 0, "<" ++ tag_name) orelse return null;
-    const content_idx = (indexOf(u8, gpx, from, ">") orelse return null) + 1;
+pub fn tagContent(gpx: []const u8, comptime tag_name: []const u8) ![]const u8 {
+    const from = indexOf(u8, gpx, 0, "<" ++ tag_name) orelse return Error.InvalidGpx;
+    const content_idx = (indexOf(u8, gpx, from, ">") orelse return Error.InvalidGpx) + 1;
     const closing_tag = "</" ++ tag_name ++ ">";
-    const to = indexOf(u8, gpx, from + 1, closing_tag) orelse return null;
+    const to = indexOf(u8, gpx, from + 1, closing_tag) orelse return Error.InvalidGpx;
     return gpx[content_idx..to];
 }
 
@@ -145,30 +163,69 @@ test tagContent {
         \\</trkpt>
     ;
 
-    const ele = tagContent(gpx, "ele");
-    const time = tagContent(gpx, "time");
+    const ele = try tagContent(gpx, "ele");
+    const time = try tagContent(gpx, "time");
 
-    try testing.expect(mem.eql(u8, ele.?, "126.0"));
-    try testing.expect(mem.eql(u8, time.?, "2022-01-07T09:19:57Z"));
-    try testing.expect(tagContent("missingtag", gpx) == null);
+    try testing.expect(mem.eql(u8, ele, "126.0"));
+    try testing.expect(mem.eql(u8, time, "2022-01-07T09:19:57Z"));
+    try testing.expectError(Error.InvalidGpx, tagContent("missingtag", gpx));
+    try testing.expectError(Error.InvalidGpx, tagContent("<noclosingtag>content", "noclosingtag"));
 }
 
-pub fn attr(gpx: []const u8, attr_name: []const u8) ?[]const u8 {
-    var from = indexOf(u8, gpx, 0, attr_name) orelse return null;
+pub fn attr(gpx: []const u8, attr_name: []const u8) ![]const u8 {
+    var from = indexOf(u8, gpx, 0, attr_name) orelse return Error.InvalidGpx;
     from += attr_name.len + "=\"".len;
-    const to = indexOf(u8, gpx, from + 1, "\"") orelse return null;
+    const to = indexOf(u8, gpx, from + 1, "\"") orelse return Error.InvalidGpx;
     return gpx[from..to];
 }
 
 test attr {
     const gpx = "<trkpt lat=\"48.9667130\" lon=\"32.2284080\" foo=\"bar\">";
 
-    const lon = attr(gpx, "lon");
-    const lat = attr(gpx, "lat");
-    const foo = attr(gpx, "foo");
+    const lon = try attr(gpx, "lon");
+    const lat = try attr(gpx, "lat");
+    const foo = try attr(gpx, "foo");
 
-    try testing.expect(mem.eql(u8, lat.?, "48.9667130"));
-    try testing.expect(mem.eql(u8, lon.?, "32.2284080"));
-    try testing.expect(mem.eql(u8, foo.?, "bar"));
-    try testing.expect(attr("missing", gpx) == null);
+    try testing.expect(mem.eql(u8, lat, "48.9667130"));
+    try testing.expect(mem.eql(u8, lon, "32.2284080"));
+    try testing.expect(mem.eql(u8, foo, "bar"));
+    try testing.expectError(Error.InvalidGpx, attr("missing", gpx));
+    try testing.expectError(Error.InvalidGpx, attr("<tag some=\"val  ", "some"));
+}
+
+const daysToMonth = [_]u9{ 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
+pub const time_format = "yyyy-mm-ddThh:mm:ssZ";
+const epoch = std.time.epoch;
+const parseUint = std.fmt.parseUnsigned;
+const isLeap = epoch.isLeapYear;
+const seconds_in_day = 86_400;
+
+pub fn timeStrToTimestamp(t: []const u8) !u32 {
+    if (t.len != time_format.len) return Error.InvalidGpxTime;
+    const year = try parseUint(u16, t[0..4], 10);
+    if (year < 1970) return Error.InvalidGpxTime;
+    const month = try parseUint(u4, t[5..7], 10);
+    if (month < 1 or month > 12) return Error.InvalidGpxTime;
+    const day = try parseUint(u5, t[8..10], 10);
+    const hour = try parseUint(u6, t[11..13], 10);
+    const minute = try parseUint(u6, t[14..16], 10);
+    const second = try parseUint(u6, t[17..19], 10);
+
+    var timestamp: u32 = 0;
+    for (1970..year) |y| {
+        timestamp += @as(u32, epoch.getDaysInYear(@intCast(y))) * seconds_in_day;
+    }
+    var day_index = daysToMonth[month - 1];
+    if (month > 2 and isLeap(year)) day_index += 1;
+    day_index += day - 1;
+
+    timestamp += @as(u32, day_index) * seconds_in_day;
+
+    const seconds: u17 = @as(u17, hour) * 3600 + @as(u17, minute) * 60 + second;
+    return timestamp + seconds;
+}
+
+test timeStrToTimestamp {
+    try testing.expect(try timeStrToTimestamp("2005-11-15T22:42:05Z") == 1132094525);
+    try testing.expect(try timeStrToTimestamp("1995-02-19T21:05:00Z") == 793227900);
 }
